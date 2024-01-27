cdb83a1a19e712d951a82688bd2fedd72e74120b
David Senoner <seda18@rolmail.net>
2024-01-27 17:52:52 +0100

    add --selected command to print selected profile

--- logi-profile.sh
+++ logi-profile.sh
@@ -2,6 +2,47 @@
 MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 
 array=()
+PARAM1=${1:-"NONE"}
+
+function main() {
+  getProfiles
+
+  if [ "$PARAM1" == "--list" ] || [ "$PARAM1" == "-l" ]; then
+    echo "Available profiles:"
+    for item in "${array[@]}"; do
+      echo "  ${item}"
+    done
+
+    return
+  fi
+
+
+  if [ "$PARAM1" == "--selected" ] || [ "$PARAM1" == "-s" ]; then
+    echo "Selected profile: $(cat ./selected)"
+
+    return
+  fi
+
+
+  contains="0"
+  for i in "${array[@]}"; do
+    if [ "$i" == "$PARAM1" ]; then
+      contains="1"
+    fi
+  done
+
+  if [ "$contains" == "0" ]; then
+    echo "There exists no profile called '${PARAM1}'"
+  else
+    echo "Setting profile..."
+    if [ -f /etc/logid.cfg ]; then
+      sudo rm /etc/logid.cfg
+    fi
+    sudo cp ${MY_PATH}/profiles/${PARAM1}.cfg /etc/logid.cfg
+    sudo systemctl restart logid
+    echo "$PARAM1" > ./selected
+  fi
+}
 
 function getProfiles() {
   search_dir=$MY_PATH/profiles
@@ -21,29 +62,6 @@ function getProfiles() {
   done
 }
 
-  getProfiles
 
-  if [ "$1" == "--list" ] || [ "$1" == "-l" ]; then
-    echo "Available profiles:"
-    for item in "${array[@]}"; do
-      echo "  ${item}"
-    done
-  else
-    contains="0"
-    for i in "${array[@]}"; do
-      if [ "$i" == "$1" ]; then
-        contains="1"
-      fi
-    done
-
-    if [ "$contains" == "0" ]; then
-      echo "There exists no profile called '${1}'"
-    else
-      echo "Setting profile..."
-      if [ -f /etc/logid.cfg ]; then
-        sudo rm /etc/logid.cfg
-      fi
-      sudo cp ${MY_PATH}/profiles/${1}.cfg /etc/logid.cfg
-      sudo systemctl restart logid
-    fi
-  fi
+  
+main "$@"
